46ba4c2d32f3dde7f445b2fb2c947aa8f7ea7553
Mason Drabik <mkdrabik@unc.edu>
2022-09-08 18:00:55 -0400

    lessons

--- lessons/love_functions.py
+++ lessons/love_functions.py
@@ -4,4 +4,15 @@ def love(subject: str) ->str:
     """"Given a subject as a parameter, returns a loving string. """ 
     return f"I love you {subject}! " 
 
-print(love(input("Enter a subject ")))
+#print(love(input("Enter a subject ")))
+
+def spread_love(to: str, n: int) -> str:
+    """Generates a str repeating a loving message n times."""
+    love_note: str = ""
+    i: int = 0
+    while i < n: 
+        love_note= love_note + love(to) + "\n"
+        i += 1
+    return love_note
+
+
