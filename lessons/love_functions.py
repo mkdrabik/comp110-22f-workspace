f71320a9a4c78e69d38bf211150d2ab1d726d223
Mason Drabik <mkdrabik@unc.edu>
2022-09-08 14:12:56 -0400

    lessons

---
+++ lessons/love_functions.py
@@ -0,0 +1,7 @@
+""""Some tender, loving functions"""
+
+def love(subject: str) ->str:
+    """"Given a subject as a parameter, returns a loving string. """ 
+    return f"I love you {subject}! " 
+
+print(love(input("Enter a subject ")))
