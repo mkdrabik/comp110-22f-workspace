c7aa74e39f2957adea8816174e972662d684b070
Mason Drabik <mkdrabik@unc.edu>
2022-09-13 22:47:53 -0400

    ex04

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,52 @@
+"""Working with lists!!!"""
+__author__ = "730554383"
+
+
+def all(numbers: list[int], num: int) -> bool:
+    """Transverses a list and returns if all the ints in the list are the same as the given"""
+    i: int = 0
+    same: bool = True
+
+    while i < len(numbers)-1:
+        if numbers[i] == num:
+            i += 1
+        else:
+            same = False
+            i = len(numbers)
+    return same
+
+
+def max(values: list[int]) -> int:
+    """Finds max value in a list"""
+    if len(values) == 0:
+        raise ValueError("max() arg is an empty List")
+
+    max: int = values[0]
+    ind: int = 0
+
+    while ind < len(values):
+        if max <= values[ind]:
+            max = values[ind]
+            ind += 1
+        else:
+            ind += 1
+    return max
+
+
+def is_equal(user_values1: list[int], user_values2: list[int]) -> bool:
+    """Transverses both lists to see if each value equals each other"""
+
+    v: int = 0
+    equal: bool = True
+
+    while v < len(user_values1)-1:
+        if user_values1[v] == user_values2[v]:
+            v += 1 
+        else:
+            equal = False
+            v = len(user_values1)
+    return equal
+
+
+
+
