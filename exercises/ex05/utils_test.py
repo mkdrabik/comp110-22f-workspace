0054c6f65a64cd3f11a5485b566190609d5a899e
Mason Drabik <mkdrabik@unc.edu>
2022-09-21 12:58:13 -0400

    1st tests done

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,4 +1,22 @@
 """Unit testing functons."""
 __author__ = "730554383"
 
+from utils import only_evens
 
+
+def test_list_empty() -> None:
+    """Tests to ensure that an empty list returns an empty list."""
+    xs: list[int] = list()
+    assert only_evens(xs) == []
+
+
+def test_list_functionality() -> None:
+    """Tests to ensure that the function returns a list of even values."""
+    xs: list[int] = [10, 11, 12, 13]
+    assert only_evens(xs) == [10, 12]
+
+
+def test_list_no_evens() -> None:
+    """Tests to ensure a list with no evens returns empty."""
+    xs: list[int] = [11, 13, 15]
+    assert only_evens(xs) == []
