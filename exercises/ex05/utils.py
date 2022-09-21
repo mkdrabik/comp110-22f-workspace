c5ab21890af909cda9d5ea057608e6ebb2fdc567
Mason Drabik <mkdrabik@unc.edu>
2022-09-21 13:11:14 -0400

    again

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,15 +2,15 @@
 __author__ = "730554383"
 
 
-
 def only_evens(xs: list[int]) -> list[int]:
     """Given a list of ints returns a new list of only even values."""
     nlist: list[int] = list()
     
     if len(xs) == 0:
         return nlist
-        
+
     i: int = 0
+
     while i < len(xs):
         if xs[i] % 2 == 0:
             nlist.append(xs[i])
@@ -22,14 +22,18 @@ def concat(xs: list[int], xy: list[int]) -> list[int]:
     """Given two lists of ints returns a new list made of the values of both lists."""
     nlist: list[int] = list()
     i: int = 0
-    while i < len(xs):
-        nlist.append(xs[i])
-        i += 1 
+    
+    if len(xs) > 0: 
+        while i < len(xs):
+            nlist.append(xs[i])
+            i += 1 
     
     i = 0
-    while i < len(xy):
-        nlist.append(xy[i])
-        i += 1
+
+    if len(xy) > 0:
+        while i < len(xy):
+            nlist.append(xy[i])
+            i += 1
     return nlist
 
 
@@ -38,9 +42,12 @@ def sub(xs: list[int], low: int, high: int) -> list[int]:
     nlist: list[int] = list()
     if len(xs) == 0 or low > len(xs) or high <= 0:
         return nlist
+
     i: int = low
+
     if low < 0:
         i = 0
+
     while i < high - 1 and i < len(xs):
         nlist.append(xs[i])
     return nlist
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,7 +1,7 @@
 """Unit testing functons."""
 __author__ = "730554383"
 
-from utils import only_evens
+from utils import only_evens, concat, sub
 
 
 def test_list_empty() -> None:
@@ -20,3 +20,30 @@ def test_list_no_evens() -> None:
     """Tests to ensure a list with no evens returns empty."""
     xs: list[int] = [11, 13, 15]
     assert only_evens(xs) == []
+
+
+def test_both_lists_full() -> None:
+    """Tests to make sure an empty list is returned if both are empty."""
+    xs: list[int] = list()
+    xy: list[int] = list()
+    assert concat(xs, xy) == []
+
+
+def test_one_full_one_empty() -> None: 
+    """Tests that a list is formed even if one list is empty"""
+    xs: list[int] = [1, 2, 3]
+    xy: list[int] = list()
+    assert concat(xs, xy) == [1, 2, 3]
+
+
+def test_both_full() -> None:
+    """Tests to see that a list is formed when both lists are full."""
+    xs: list[int] = [1, 2, 3]
+    xy: list[int] = [4, 5, 6]
+    assert concat(xs, xy) == [1, 2, 3, 4, 5, 6]
+
+
+def test_boudns() -> None:
+    """Tests to see that a list is returned when the low argument is negative and high argument is higher then the length of the list."""
+    xs: list[int] = [1, 2, 3]
+    assert sub(xs, -1, 4) == [1, 2, 3]
