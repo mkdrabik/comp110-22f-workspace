826900c177acf3808390125d432a55bf12795dc9
Mason Drabik <mkdrabik@unc.edu>
2022-09-27 12:59:13 -0400

    stuff

--- lessons/for_in.py
+++ lessons/for_in.py
@@ -3,6 +3,7 @@
 names: list[str] = ["Kris, Mason, Riley, Arun"]
 
 # Example of iterating through names using while
+print("While output")
 i: int = 0
 while i < len(names):
     name: str = names[i]
@@ -10,5 +11,6 @@ while i < len(names):
     i += 1
 
 # The following for..in loop is the same as the while loop
+print("for..in output")
 for name in names:
     print(name)
