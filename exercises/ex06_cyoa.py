bfa9ff998f2a34c69443af64e44c51f654445acc
Mason Drabik <mkdrabik@unc.edu>
2022-09-28 12:51:37 -0400

    yuh

--- exercises/ex06_cyoa.py
+++ exercises/ex06_cyoa.py
@@ -2,78 +2,82 @@
 __author__ = "730554383"
 
 from random import randint
-PLAYER_NAME: str = input("Enter your name. ")
-ENEMY_NAME: str = input("Enter a creature you would like to fight. ")
+player: str = ""
+enemy: str = ""
 points: int = 5
 enemy_points: int = 5
 ENEMY_ATTACK_OPTIONS: list[str] = ["kick", "tackle", "punch"]
 
 
 def greet() -> None:
-    """Greeting to the player and a introduction to the game."""   
-    print(f"Welcome {PLAYER_NAME}, today you will be battling a {ENEMY_NAME}")
-    print(f"You currently have {points} points and the {ENEMY_NAME} has {enemy_points} points")
+    """Greeting to the player and a introduction to the game."""
+    global player
+    global enemy
+    player = input("Enter your name: ")   
+    enemy = input("Enter a creature you would like to fight: ")
+    print(f"Welcome {player}, today you will be battling a {enemy}")
+    print(f"You currently have {points} points and the {enemy} has {enemy_points} points")
     print("You have three options to attack: punch, kick, and tackle. Punch beats kick, tackle beats punch, and kick beats tackle.")
-    print("You can also run away, but you forfeit the match and end the game.")
-    print(f"The goal is to outlast the {ENEMY_NAME} and win the game, good luck.")
+    print("You can also run away, but you lose the fight and end the game.")
+    print(f"The goal is to outlast the {enemy} and win the game, good luck.")
 
 
 def kick() -> None:
     """Option when user chooses kick."""
     global enemy_points
     global points
-    print(f"{PLAYER_NAME} has chosen to kick.")
+    print(f"{player} has chosen to kick.")
     i: int = randint(1, 2)
     attack: str = ENEMY_ATTACK_OPTIONS[i]
     if attack == "tackle":
-        print(f"{PLAYER_NAME} kicks the {ENEMY_NAME} hurting it")
+        print(f"{player} kicks the {enemy} hurting it.")
         enemy_points -= 1
-        print(f"Current scores: {PLAYER_NAME} = {points} || {ENEMY_NAME} = {enemy_points}")
+        print(f"Current scores: {player} = {points} || {enemy} = {enemy_points}")
     else:
-        print(f"{ENEMY_NAME} attacks {PLAYER_NAME} with a {attack} and lands a blow")
+        print(f"{enemy} attacks {player} with a {attack} and hurts {player}")
         points -= 1
-        print(f"Current scores: {PLAYER_NAME} = {points} || {ENEMY_NAME} = {enemy_points}")
+        print(f"Current scores: {player} = {points} || {enemy} = {enemy_points}")
 
 
 def punch() -> None:
     """Option when user chooses punch."""
     global enemy_points
     global points
-    print(f"{PLAYER_NAME} has chosen to punch.")
+    print(f"{player} has chosen to punch.")
     i: int = randint(0, 1)
     attack: str = ENEMY_ATTACK_OPTIONS[i]
     if attack == "kick":
-        print(f"{PLAYER_NAME} punches the {ENEMY_NAME} hurting it")
+        print(f"{player} punches the {enemy} hurting it")
         enemy_points -= 1
-        print(f"Current scores: {PLAYER_NAME} = {points} || {ENEMY_NAME} = {enemy_points}")
+        print(f"Current scores: {player} = {points} || {enemy} = {enemy_points}")
     else:
-        print(f"{ENEMY_NAME} attacks {PLAYER_NAME} with a {attack} and lands a blow")
+        print(f"{enemy} attacks {player} with a {attack} and hurts {player}")
         points -= 1
-        print(f"Current scores: {PLAYER_NAME} = {points} || {ENEMY_NAME} = {enemy_points}")
+        print(f"Current scores: {player} = {points} || {enemy} = {enemy_points}")
 
 
 def tackle() -> None:
     """Option when user chooses tackle."""
     global enemy_points
     global points
-    print(f"{PLAYER_NAME} has chosen to tackle.")
+    print(f"{player} has chosen to tackle.")
     options: list[int] = [0, 2]
     i: int = randint(0, 1)
     attack: str = ENEMY_ATTACK_OPTIONS[options[i]]
     if attack == "punch":
-        print(f"{PLAYER_NAME} tackles the {ENEMY_NAME} hurting it")
+        print(f"{player} tackles the {enemy} hurting it")
         enemy_points -= 1
-        print(f"Current scores: {PLAYER_NAME} = {points} || {ENEMY_NAME} = {enemy_points}")
+        print(f"Current scores: {player} = {points} || {enemy} = {enemy_points}")
     else:
-        print(f"{ENEMY_NAME} attacks {PLAYER_NAME} with a {attack} and lands a blow")
+        print(f"{enemy} attacks {player} with a {attack} and hurts {player}")
         points -= 1
-        print(f"Current scores: {PLAYER_NAME} = {points} || {ENEMY_NAME} = {enemy_points}")
+        print(f"Current scores: {player} = {points} || {enemy} = {enemy_points}")
 
 
 def run_away() -> None:
     """Options when user wants to quit. """
     global points
-    print(f"{PLAYER_NAME} has chosen to run away and there for loses the game")
+    print(f"{player} has chosen to run away with {points} points.")
     points = 0
 
 
@@ -95,10 +99,10 @@ def main() -> None:
         else:
             attack_choice = input("Not a valid answer, please enter another choice: ")
     if points == 0:
-        print("You have lost the fight")
+        print(f"{player} lost the fight")
         quit()
     else: 
-        print(f"{PLAYER_NAME} has won the fight with {points} points left, congrats. ")
+        print(f"{player} has won the fight with {points} points left, congrats. ")
         quit()
     
 
