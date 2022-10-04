79839c72c5797b83924cbe9780f730bd3791da9e
Mason Drabik <mkdrabik@unc.edu>
2022-10-04 15:15:47 -0400

    eh

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -5,6 +5,27 @@ __author__ = "730554383"
 def invert(x: dict[str, str]) -> dict[str, str]:
     """Returns an inverted version of the inputed dicionary where the key becomes the value and vice versa"""
     names: dict[str, str] = {}
+    vals: list[str] = list()
+    i: int = 0
+    v: int = 0
+    for y in x:
+        vals.append(x[y])
+    
+    while i < len(vals):
+        v = i + 1
+        while v < len(vals):
+            if vals[i] == vals[v]:
+                raise KeyError
+            v += 1
+        i += 1
+    
     for i in x:
         names[x[i]] = i
     return names
+
+
+
+    
+
+
+
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -2,12 +2,12 @@
 __author__ = "730554383"
 
 from dictionary import invert
-
+import pytest
 
 def test_is_inverted() -> None:
     """Tests to ensure the function works with a normal argument."""
-    xs: dict[str, str] = {"Mason": "Drabik", "Kris": "Jordan", "Michael": "Jordan"}
-    assert invert(xs) == {"Drabik": "Mason", "Jordan": "Kris", "Jordan": "Michael"}
+    xs: dict[str, str] = {"Mason": "Drabik", "Kris": "Jordan", "Michael": "Jeff"}
+    assert invert(xs) == {"Drabik": "Mason", "Jordan": "Kris", "Jeff": "Michael"}
 
 def test_empty_dictionary() -> None:
     """Tests to ensure the function works with an empty dict."""
@@ -17,5 +17,6 @@ def test_empty_dictionary() -> None:
 
 def test_one_key() -> None:
     """Tests to ensure that the function works when dict has only one key."""
-    xs: dict[str, str] = {"Mason": "Drabik"}
-    assert invert(xs) == {"Drabik": "Mason"}
+    with pytest.raises(KeyError):
+        xs: dict[str, str] = {"Mason": "Drabik", "Miles": "Drabik"}
+        invert(xs)
