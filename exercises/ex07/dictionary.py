bc22599f3819db46cd60412cdd6f939fce96af1d
Mason Drabik <mkdrabik@unc.edu>
2022-10-04 16:53:11 -0400

    yes

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -8,6 +8,7 @@ def invert(x: dict[str, str]) -> dict[str, str]:
     vals: list[str] = list()
     i: int = 0
     v: int = 0
+    
     for y in x:
         vals.append(x[y])
     
@@ -23,9 +24,61 @@ def invert(x: dict[str, str]) -> dict[str, str]:
         names[x[i]] = i
     return names
 
-
-
+ 
+def favorite_color(x: dict[str, str]) -> str:
+    """Returns a the color that appears most in a given dictionary."""
+    colors: list[str] = []
+    nums: list[int] = []  
+    i: int = 0
+    v: int = 0
+    count: int = 0
+    max: int = 0
     
+    for y in x:
+        colors.append(x[y])
+    
+    while i < len(colors):
+        v = 0
+        while v < len(colors):
+            if colors[i] == colors[v]:
+                count += 1
+            v += 1
+        nums.append(count)
+        count = 0
+        i += 1
+    
+    i = 0
+    v = 0
+    max = nums[i]
+
+    while i < len(nums):
+        if max < nums[i]:
+            max = nums[i]
+        i += 1    
+    return colors[nums.index(max)]
 
 
+def count(x: list[str]) -> dict[str, int]:
+    """Returns a dictionary based on how many times a string appears in a list."""
+    vals: dict[str, int] = {}
+    val: str = ""
+    i: int = 0
+    v: int = 0
+    appearences: int = 0
 
+    while i < len(x):
+        val = x[i]
+        v = i
+
+        if val in vals: 
+            i += 1
+        else:
+            while v < len(x):
+                if x[i] == x[v]:
+                    appearences += 1
+                v += 1
+            
+            vals[val] = appearences
+            appearences = 0
+            i += 1
+    return vals
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,13 +1,17 @@
 """Dictionary testing."""
 __author__ = "730554383"
 
-from dictionary import invert
+
+from unittest.mock import NonCallableMagicMock
+from dictionary import invert, favorite_color, count
 import pytest
+
 
 def test_is_inverted() -> None:
     """Tests to ensure the function works with a normal argument."""
     xs: dict[str, str] = {"Mason": "Drabik", "Kris": "Jordan", "Michael": "Jeff"}
     assert invert(xs) == {"Drabik": "Mason", "Jordan": "Kris", "Jeff": "Michael"}
+
 
 def test_empty_dictionary() -> None:
     """Tests to ensure the function works with an empty dict."""
@@ -20,3 +24,39 @@ def test_one_key() -> None:
     with pytest.raises(KeyError):
         xs: dict[str, str] = {"Mason": "Drabik", "Miles": "Drabik"}
         invert(xs)
+
+
+def test_normal_funcationality_favorite_colors() -> None:
+    """Tests to ensure normal functionality of the function."""
+    xs: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue", "Mason": "green"}
+    assert favorite_color(xs) == "blue"
+
+
+def test_two_maxes() -> None:
+    """Tests to ensure that the first of the two highest counted colors appear."""
+    xs: dict[str, str] = {"Mason": "blue", "Miles": "yellow", "Ciel": "yellow", "Luke": "blue"}
+    assert favorite_color(xs) == "blue"
+
+
+def test_one_color() -> None:
+    """Tests to ensure if only one color is present that color is returned."""
+    xs: dict[str, str] = {"Mason": "blue"}
+    assert favorite_color(xs) == "blue"
+
+
+def test_normal_input() -> None:
+    """Tests to ensure function works properly with a normal input."""
+    xs: list[str] = ["a", "a", "a", "b", "b", "c"]
+    assert count(xs) == {"a": 3, "b": 2, "c": 1}
+
+
+def test_all_equal() -> None:
+    """Tests to ensure if every value is equal function still works properly."""
+    xs: list[str] = ["a", "b", "c"]
+    assert count(xs) == {"a": 1, "b": 1, "c": 1}
+
+
+def test_empty() -> None:
+    """Tests to ensure an empty dictionary is returned if list is empty."""
+    xs: list[int] = []
+    assert count(xs) == {}
