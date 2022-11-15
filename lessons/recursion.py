6ce971edd4a49c3f13b6213ae005877b149eaac9
Mason Drabik <mkdrabik@unc.edu>
2022-11-15 13:12:57 -0500

    kaj

---
+++ lessons/recursion.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import Optional
+
+class Node:
+    data: int
+    next: Union[Node, None]
+
+    def __init__(self, data: int, next: Optional[Node]):
+        self.data = data
+        self.next = next
+
+    def __str__(self) -> str:
+        if self.next == None:
+            return f"{self.data} -> None"
+        else:
+            return f"{self.data} -> {self.next}"
+
+
+def sum(node: Optional[Node]) -> int:
+    if node.next == None:
+        return node.data
+    else: 
+        return node.data + sum(node.next)
+
+
+def count(node: Node, current_count: int = 0) -> int:
+    if node.next == None:
+        return current_count + 1
+    else:
+        return count(node.next, current_count + 1)
+
+
+head: Node = Node(3, None)
+head: Node = Node(2, head)
+head = Node(1, head)
+print(sum(head))
+print(count(head))
+print(head)
