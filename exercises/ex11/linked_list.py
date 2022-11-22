5293c161e6595236f041e2532a7a6890170db090
Mason Drabik <mkdrabik@unc.edu>
2022-11-22 14:31:10 -0500

    done

---
+++ exercises/ex11/linked_list.py
@@ -0,0 +1,89 @@
+"""Utility functions for working with Linked Lists."""
+
+from __future__ import annotations
+from typing import Optional
+
+__author__ = "730554383"
+
+
+class Node:
+    """An item in a singly-linked list."""
+    data: int
+    next: Optional[Node]
+
+    def __init__(self, data: int, next: Optional[Node]):
+        """Construct a singly linked list. Use None for 2nd argument if tail."""
+        self.data = data
+        self.next = next
+
+    def __str__(self) -> str:
+        """Produce a string visualization of the linked list."""
+        if self.next is None:
+            return f"{self.data} -> None"
+        else:
+            return f"{self.data} -> {self.next}"
+
+
+def is_equal(lhs: Optional[Node], rhs: Optional[Node]) -> bool:
+    """Test if two linked lists are deeply (values and order) equal to one another."""
+    if lhs is None and rhs is None:
+        return True
+    elif lhs is None or rhs is None or lhs.data != rhs.data:
+        return False
+    else:
+        return is_equal(lhs.next, rhs.next)
+
+
+def last(head: Optional[Node]) -> int:
+    """Returns the last value of a Linked List, or raises a ValueError if the list is empty."""
+    if head is None:
+        raise ValueError("last cannot be called with None")
+    elif head.next == None:
+        return head.data
+    else:
+        return last(head.next)
+
+
+def value_at(head: Optional[Node], index: int) -> int:
+    """Returns data of the Node at the given index."""
+    if index < 0 or head is None:
+        raise IndexError("Index is out of bounds on the list.")
+    elif index == 0:
+        return head.data
+    else:
+        return value_at(head.next, index - 1)
+
+
+def max(head: Optional[Node]) -> int:
+    """Returns highest value in linked list."""
+    if head is None:
+        raise ValueError("Cannot call max with None.")
+    elif head.next is None:
+        return head.data
+    else:
+        i: int = max(head.next)
+        if head.data > i:
+            i = head.data
+        return i
+         
+
+def linkify(items: list[int]) -> Optional[Node]:
+    """Returns a linked list with the same int values as the given list."""
+    if items == []:
+        return None
+    else:
+        n: Node = Node(items[0], linkify(items[1:]))
+        return n
+
+
+def scale(head: Optional[Node], factor: int) -> Optional[Node]:
+    """Scales a given node by a factor."""
+    if head is None:
+        return None
+    elif head.next is None:
+        head.data *= factor
+        return head
+    else:
+        n: Node = Node(head.data * factor, scale(head.next, factor))
+        return n
+        
---
+++ exercises/ex11/linked_list_test.py
@@ -0,0 +1,62 @@
+"""Tests for linked list utils."""
+
+import pytest
+from exercises.ex11.linked_list import Node, last, value_at, max, linkify, scale
+
+__author__ = "730554383"
+
+
+def test_last_empty() -> None:
+    """Last of an empty Linked List should raise a ValueError."""
+    with pytest.raises(ValueError):
+        last(None)
+
+
+def test_last_non_empty() -> None:
+    """Last of a non-empty list should return its last data value."""
+    linked_list = Node(1, Node(2, Node(3, None)))
+    assert last(linked_list) == 3
+
+
+def test_index_ob() -> None:
+    """Tests to see that error is raised when index is wrong."""
+    with pytest.raises(IndexError):
+        linked_list = Node(1, Node(2, Node(3, None)))
+        value_at(linked_list, 4)
+
+    
+def test_finds_value() -> None:
+    """Tests to find value at Index."""
+    assert value_at(Node(10, Node(20, Node(30, None))), 2) == 30
+
+
+def test_finds_max() -> None:
+    """Tests to ensure that the max value is found."""
+    linked_list = Node(10, Node(30, Node(20, None)))
+    assert max(linked_list) == 30
+
+
+def test_empty() -> None:
+    """Tests to ensure nothing happens when list is empty."""
+    with pytest.raises(ValueError):
+        max(None)
+
+
+def test_links() -> None:
+    """Tests to enure that the list is linked properly."""
+    assert str(linkify([1, 2, 3])) == "1 -> 2 -> 3 -> None"
+
+
+def test_empty_links() -> None:
+    """Test to ensure none is returned if list is empty."""
+    assert linkify([]) == None
+
+
+def test_scale() -> None:
+    """Tests to ensure scaling is done properly."""
+    assert str(scale(linkify([1, 2, 3]), 2)) == "2 -> 4 -> 6 -> None"
+
+
+def test_empty_scale() -> None:
+    """Tests to ensure None is returned when linkfy is empty."""
+    assert scale(linkify([]), 2) == None
